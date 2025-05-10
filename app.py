c3cf63f11c594def060086fa1a7e4b09a5c721d5
Saurabh Kuche <136221667+Sau2003@users.noreply.github.com>
2025-05-10 17:33:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+movies_dict = pickle.load(open("movies_dict.pkl", "rb"))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+
+st.title("Movie Recommender System")
+
+selected_movie = st.selectbox("Select a movie", movies['title'].values)
+
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie)
+    for movie in recommendations:
+        st.write(movie)
